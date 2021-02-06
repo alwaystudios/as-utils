0b32f3a74d508030c3aa7315f316f31df108316d
Gary Alway <garyalway@gmail.com>
2021-02-06 11:00:28 +0000

    1.0.16

--- src/taskQueue.ts
+++ src/taskQueue.ts
@@ -3,33 +3,33 @@ import { EventEmitter } from 'events'
 export type Task<T> = () => Promise<T>
 
 export class TaskQueue<T> extends EventEmitter {
-  concurrency: number
-  running: number
-  queue: Task<T>[]
+  #concurrency: number
+  #running: number
+  #queue: Task<T>[]
 
   constructor(concurrency: number) {
     super()
-    this.concurrency = concurrency
-    this.running = 0
-    this.queue = []
+    this.#concurrency = concurrency
+    this.#running = 0
+    this.#queue = []
   }
 
   next() {
-    if (this.running === 0 && this.queue.length === 0) {
+    if (this.#running === 0 && this.#queue.length === 0) {
       return this.emit('empty')
     }
 
-    while (this.running < this.concurrency && this.queue.length) {
+    while (this.#running < this.#concurrency && this.#queue.length) {
       // eslint-disable-next-line functional/immutable-data
-      const task = this.queue.shift()!
+      const task = this.#queue.shift()!
       // eslint-disable-next-line functional/immutable-data
-      this.running++
+      this.#running++
       task()
         .then((res) => this.emit('complete', res))
         .catch((err) => this.emit('error', err))
         .finally(() => {
           // eslint-disable-next-line functional/immutable-data
-          this.running--
+          this.#running--
           process.nextTick(this.next.bind(this))
         })
     }
@@ -39,7 +39,7 @@ export class TaskQueue<T> extends EventEmitter {
 
   addTask(task: Task<T>) {
     // eslint-disable-next-line functional/immutable-data
-    this.queue.push(task)
+    this.#queue.push(task)
     process.nextTick(this.next.bind(this))
     return this
   }
